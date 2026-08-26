dc245a40cc1c7b59bcaf511440117d4e20a54878
Hamad tarawa <tourt2031@gmail.com>
2026-08-26 18:56:22 +0300

    chore: add MCP server application entry point

---
+++ src/main/java/com/hamooda/hotelmcp/HotelManagementMcpApplication.java
@@ -0,0 +1,12 @@
+package com.hamooda.hotelmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementMcpApplication.class, args);
+    }
+}
