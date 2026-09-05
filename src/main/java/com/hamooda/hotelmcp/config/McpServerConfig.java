a5812681cea9d51c7f2c55c30dbe94c7047150ca
Hamad tarawa <tourt2031@gmail.com>
2026-09-05 12:03:57 +0300

    refactor: remove hotel search prompt specification from configuration

--- src/main/java/com/hamooda/hotelmcp/config/McpServerConfig.java
+++ src/main/java/com/hamooda/hotelmcp/config/McpServerConfig.java
@@ -24,50 +24,5 @@ public class McpServerConfig {
                 .toolObjects(hotelSearchTool, registerUserTool, hotelDeletionTool)
                 .build();
     }
-    @Bean
-    public List<McpServerFeatures.SyncPromptSpecification> hotelPrompts() {
 
-        var hotelSearchPrompt = new McpSchema.Prompt(
-                "hotelSearch",
-                "A prompt to search for hotels",
-                List.of(
-                        new McpSchema.PromptArgument(
-                                "destination",
-                                "The destination to search for",
-                                true
-                        )
-                )
-        );
-
-        var hotelSearchPromptSpec =
-                new McpServerFeatures.SyncPromptSpecification(
-                        hotelSearchPrompt,
-                        (exchange, getPromptRequest) -> {
-
-                            String destination =
-                                    (String) getPromptRequest.arguments()
-                                            .get("destination");
-
-                            var userMessage = new McpSchema.PromptMessage(
-                                    McpSchema.Role.USER,
-                                    new McpSchema.TextContent(
-                                            String.format(
-                                                    "Find available hotels in %s",
-                                                    destination
-                                            )
-                                    )
-                            );
-
-                            return new McpSchema.GetPromptResult(
-                                    String.format(
-                                            "A prompt to search for hotels in %s",
-                                            destination
-                                    ),
-                                    List.of(userMessage)
-                            );
-                        }
-                );
-
-        return List.of(hotelSearchPromptSpec);
-    }
 }
